10bdb381f358561c7bd6485c5a4322b9407f6e20
Strimmerz <strimme98@gmail.com>
2024-01-23 20:41:22 +0530

    update sub

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 def add(a,b,c):
     return a+b+c
 
-<<<<<<< HEAD
 def sub(a,b,e)
     return a-b-e
-=======
-def sub(a,b,d):
-    return a-b-d
->>>>>>> 44aca86ce404a928bbb9a32efe4230f61a426eb7
 
 def div(a,b):
     return a/b
